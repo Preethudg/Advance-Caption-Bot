c0618e693b849b53c43361b49469cbd969118db3
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-07 11:00:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,12 +14,12 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", '2089142494'))
+ADMIN = int(getenv("ADMIN", "2089142494"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
 API_ID = int(getenv("API_ID", "23009724"))
 API_HASH = str(getenv("API_HASH", "66a87d259426e268bdd765fb4c635f2b"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1002151971318") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002478702126") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://hi:Preethu@cluster0.vpnst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
