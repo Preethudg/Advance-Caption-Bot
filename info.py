3818a708bad1054bb16d4578ed1e4e8c4467c58a
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-14 11:37:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,13 +14,13 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", "7405406082"))
+ADMIN = int(getenv("ADMIN", "8096214375"))
 SILICON_PIC = os.environ.get("SILICON_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
-API_ID = int(getenv("API_ID", "23009724"))
-API_HASH = str(getenv("API_HASH", "66a87d259426e268bdd765fb4c635f2b"))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "0") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://hi:Preethu@cluster0.vpnst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
+API_ID = int(getenv("API_ID", "22237124"))
+API_HASH = str(getenv("API_HASH", "3ed92051ab8064684d8b1a5babd79ec8")
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7579794242:AAEIPHd7b8OAfNuBfYsdSKsGNLWn2RepxDo"))
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1002220670718") 
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://kannadam851:GNd1AEngyHEn6rVZ@cluster0.ksecq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
